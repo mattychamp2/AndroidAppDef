7f02b758171c5db3d5044e84d53c7256451c47f6
timv <tim.verdickt@student.kuleuven.be>
2022-05-02 14:46:52 +0200

    test

--- app/src/main/java/be/kuleuven/androidapp/MainActivity.java
+++ app/src/main/java/be/kuleuven/androidapp/MainActivity.java
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+//push-test 1 2 1 2
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
