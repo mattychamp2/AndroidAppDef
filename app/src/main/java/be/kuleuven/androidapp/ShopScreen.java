b7591893b7d4aa1f6bea27fba36bc09e62c9b8d4
LAPTOP-QVRV3CPP\verni <vernimme.matthias@gmail.com>
2022-05-03 17:27:22 +0200

    ListView probeersel

--- app/src/main/java/be/kuleuven/androidapp/ShopScreen.java
+++ app/src/main/java/be/kuleuven/androidapp/ShopScreen.java
@@ -5,30 +5,66 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.widget.ActionMenuView;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import java.util.ArrayList;
+
 public class ShopScreen extends AppCompatActivity {
 
-    private ListView l;
-    private String[] tutorials =
-             { "Brood", "Nog brood",
-            "Meer brood", "koek"};
-
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shop_screen);
-        l = findViewById(R.id.lstView);
-        ArrayAdapter<String> arr;
-        arr
-                = new ArrayAdapter<String>(
+
+        ListView listview = (ListView) findViewById(R.id.lstView);
+        listview.setOnItemClickListener(this::onItemClick);
+
+        ArrayList<String> arrayLst = new ArrayList<String>();
+        arrayLst.add("foo");
+        arrayLst.add("bar");
+        arrayLst.add("foo");
+        arrayLst.add("bar");
+        arrayLst.add("foo");
+        arrayLst.add("bar");
+        arrayLst.add("foo");
+        arrayLst.add("bar");
+        arrayLst.add("bar");
+        arrayLst.add("foo");
+        arrayLst.add("bar");
+        arrayLst.add("foo");
+        arrayLst.add("bar");
+        arrayLst.add("foo");
+        arrayLst.add("bar");
+        arrayLst.add("foo");
+        arrayLst.add("bar");
+        arrayLst.add("foo");
+        arrayLst.add("bar");
+        arrayLst.add("bar");
+        arrayLst.add("foo");
+        arrayLst.add("bar");
+
+
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(
                 this,
-                androidx.appcompat.R.layout.support_simple_spinner_dropdown_item,
-                tutorials);
-        l.setAdapter(arr);
+                android.R.layout.simple_list_item_1,
+                arrayLst );
+
+        listview.setAdapter(arrayAdapter);
+
+    }
+
+    public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+        Intent intent = new Intent();
+        intent.setClass(this, ProductPage.class);
+        intent.putExtra("position", position);
+        // Or / And
+        intent.putExtra("id", id);
+        startActivity(intent);
     }
 
 }
