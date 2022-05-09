e4b30830b6949776fe2f0c3c278d3568c7c6697d
LAPTOP-QVRV3CPP\verni <vernimme.matthias@gmail.com>
2022-05-09 17:17:06 +0200

    probeersel botton nav

---
+++ app/src/main/java/be/kuleuven/androidapp/Shop.java
@@ -0,0 +1,18 @@
+package be.kuleuven.androidapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import be.kuleuven.androidapp.databinding.ActivityMainBinding;
+
+public class Shop extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_template);
+    }
+}
