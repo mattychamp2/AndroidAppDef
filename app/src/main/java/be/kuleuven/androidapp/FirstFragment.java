1d91333f57c1465d83fa604d4f0ccbb3336e234d
LAPTOP-QVRV3CPP\verni <vernimme.matthias@gmail.com>
2022-05-25 11:50:09 +0200

    deze ook nog

--- app/src/main/java/be/kuleuven/androidapp/FirstFragment.java
+++ app/src/main/java/be/kuleuven/androidapp/FirstFragment.java
@@ -8,7 +8,6 @@ import android.util.Base64;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
 
 import java.util.ArrayList;
 
